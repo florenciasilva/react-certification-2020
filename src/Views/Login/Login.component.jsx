534b9904be6e43ae251ccff88fb8c7234e5b8f6c
florenciasilva <florencia.silva@wizeline.com>
2020-09-15 18:11:28 -0500

    fix: folder structure

---
+++ src/Views/Login/Login.component.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useLogin } from '../../hooks/useLogin';
+
+import {
+  LoginContainer,
+  TitleContainer,
+  LoginForm,
+  LoginBtn,
+  GoogleLogo,
+  Peeps,
+} from '../../styles/styles';
+import logo from '../../assets/google-icon.png';
+
+const Login = () => {
+  const { user, error, login } = useLogin();
+  const { push } = useHistory();
+
+  useEffect(() => {
+    console.log(user, '<- user useEffect');
+    if (user) {
+      push('/homepage');
+    }
+  }, [user]);
+
+  console.log(user);
+  return (
+    <LoginContainer>
+      <TitleContainer>
+        <h1>React Cert</h1>
+        <div className="subline" />
+        <Peeps />
+      </TitleContainer>
+      <LoginForm>
+        <LoginBtn type="button" onClick={login}>
+          Login with
+          <GoogleLogo src={logo} />
+        </LoginBtn>
+        <div className="subline" />
+        <p>{error || ''}</p>
+      </LoginForm>
+    </LoginContainer>
+  );
+};
+
+export default Login;
--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,13 +1,23 @@
 import firebase from 'firebase';
+import { useState } from 'react';
 
 export const useLogin = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((res) => {
-      localStorage.setItem('user', JSON.stringify(res.user));
-      localStorage.setItem('error', '');
-    })
-    .catch((err) => localStorage.setItem('error', err.message));
+  const [user, setUser] = useState();
+  const [error, setError] = useState();
+
+  // setear contexto, mandar user, err, useState de ambos
+
+  const login = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((res) => {
+        // localStorage.setItem('user', JSON.stringify(res.user));
+        setUser(res.user);
+      })
+      .catch((err) => setError(err.message));
+  };
+
+  return { user, error, login };
 };
