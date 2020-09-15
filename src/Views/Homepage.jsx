24a5f4590d663348fdb1b11c93da9d8d96c16a27
florenciasilva <florencia.silva@wizeline.com>
2020-09-14 21:14:51 -0500

    styles: login

--- src/Views/Homepage.jsx
+++ src/Views/Homepage.jsx
@@ -1,41 +1,39 @@
 import React, { useState } from 'react';
-//import { useSearchAPI } from '../hooks/useSearchAPI';
+// import { useSearchAPI } from '../hooks/useSearchAPI';
 import { KEY } from '../firebase';
 
 const Homepage = () => {
-  const [ error, setError ] = useState('');
-  //const [ videoList,  setVideoList ] = useState([])
-  //const fetchVideos = useSearchAPI('surfing');
+  const [error, setError] = useState('');
+  // const [ videoList,  setVideoList ] = useState([])
+  // const fetchVideos = useSearchAPI('surfing');
 
   const mapVideos = (videoList) => {
-    console.log(videoList)
-    if(videoList) {
+    console.log(videoList);
+    if (videoList) {
       return videoList.map((video) => {
-        return (
-          <p>{video.snippet.title}</p>
-        )
+        return <p>{video.snippet.title}</p>;
       });
-    } else {
-      return <p>{error}</p>
     }
-  }
+    return <p>{error}</p>;
+  };
 
-  
   const getVideos = () => {
     const query = 'surfing';
     const params = `?part=snippet&maxResults=25&q=${query}&key=${KEY}`;
-    fetch('https://www.googleapis.com/youtube/v3/search' + params)
-      .then(res => res.json())
-      .then(res => res.error ? setError(res.error.message) : mapVideos(res.items))
-      .catch(err => setError(err.message))
-  }
+    fetch(`https://www.googleapis.com/youtube/v3/search${params}`)
+      .then((res) => res.json())
+      .then((res) => (res.error ? setError(res.error.message) : mapVideos(res.items)))
+      .catch((err) => setError(err.message));
+  };
 
   return (
     <>
-      <button type="button" onClick={getVideos}>Homepage</button>
+      <button type="button" onClick={getVideos}>
+        Homepage
+      </button>
       {mapVideos()}
     </>
-  )
+  );
 };
 
 export default Homepage;
--- src/Views/Login.jsx
+++ src/Views/Login.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { useLogin } from '../hooks/useLogin';
+import { LoginContainer, TitleContainer, LoginForm, LoginBtn, GoogleLogo, Peeps } from '../styles/styles';
+import logo from '../assets/google-icon.png';
 
 const Login = () => {
   const handleLogin = useLogin;
   const loginError = localStorage.getItem('error');
 
   return (
-    <form>
-      <button type="button" onClick={handleLogin}>
-        Login with Gmail
-      </button>
-      <p>{loginError || ''}</p>
-    </form>
+    <LoginContainer>
+      <TitleContainer>
+        <h1>React Cert
+        </h1>
+        <div className="subline" />
+        <Peeps></Peeps>
+      </TitleContainer>
+      <LoginForm>
+        <LoginBtn type="button" onClick={handleLogin}>
+          Login with
+          <GoogleLogo src={logo}/>
+        </LoginBtn>
+        <div className="subline" />
+        <p>{loginError || ''}</p>
+      </LoginForm>
+    </LoginContainer>
   );
 };
 
