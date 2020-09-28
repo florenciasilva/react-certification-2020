919a2f6c247098fa003ff0bfe6eec096a36e0b96
florenciasilva <florencia.silva@wizeline.com>
2020-09-28 18:01:10 -0500

    first iteration favorites menu

---
+++ src/components/UserProfile/UserProfile.component.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const UserProfile = () => {
+  const [ expanded, setExpanded ] = useState(false);
+
+  const getFavorites = () => {
+    return JSON.parse(localStorage.getItem('favorites')).map((favorite, i) => {
+      return (
+        <Link to={'/video=?' + favorite}> fav{i} </Link>
+      )
+    })
+  }
+
+
+  const handleMenu = (boolean) => {
+    setExpanded(boolean)
+  }
+
+  if(expanded) {
+    return (
+      <>
+      <button onClick={() => handleMenu(false)} type="button">Menu</button>
+      {getFavorites()}
+      </>
+    )
+  } else {
+    return (
+      <button onClick={() => handleMenu(true)} type="button">Menu</button>
+    )
+  }
+}
+
+export default UserProfile;
